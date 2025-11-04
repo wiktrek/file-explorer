862b8ab18c6e9d43b1e60b4583ee4ba488f9ce79
wiktrek <wiktor.janiszewski10@gmail.com>
2025-11-04 22:20:22 +0100

    feat: you can now delete files but going up is bugged

--- main.go
+++ main.go
@@ -11,13 +11,34 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type ViewState string
+
+const (
+	Default       ViewState = "default"
+	ConfirmDelete ViewState = "confirm"
+)
+
 type model struct {
-	files      []string
-	cursor     int
-	selected   map[int]struct{}
-	currentDir string
+	files         []string
+	cursor        int
+	selected      map[int]struct{}
+	currentDir    string
+	confirmCursor int
+	viewState     ViewState
 }
 
+func reloadDir(m model, path string) {
+	m.cursor = 0
+	if path == "" {
+		m.currentDir = path
+	}
+	m.files = loadFiles(m.currentDir)
+	m.View()
+}
+func deleteFile(path string) {
+	os.Remove(path)
+
+}
 func IsDirectory(path string) (bool, error) {
 	fileInfo, err := os.Stat(path)
 	if err != nil {
@@ -58,7 +79,6 @@ func goUp(dir string) string {
 	var split = strings.Split(dir, "/")
 	result := strings.Join(split[:len(split)-1], "/")
 
-	fmt.Println(split)
 	if len(result) <= 1 {
 		return "/"
 	}
@@ -72,6 +92,7 @@ func initialModel() model {
 		currentDir: defaultDir,
 		files:      loadFiles(defaultDir),
 		selected:   make(map[int]struct{}),
+		viewState:  Default,
 	}
 }
 
@@ -81,55 +102,68 @@ func (m model) Init() tea.Cmd {
 }
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
+		if m.viewState == Default {
+			switch msg.String() {
 
-		switch msg.String() {
+			case "ctrl+c", "q":
+				return m, tea.Quit
 
-		case "ctrl+c", "q":
-			return m, tea.Quit
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
 
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+			case "down", "j":
+				if m.cursor < len(m.files)-1 {
+					m.cursor++
+				}
+			case "d":
+				m.viewState = ConfirmDelete
+				m.confirmCursor = 1
+			case "o", " ":
+				fileDir := m.currentDir + m.files[m.cursor]
+				openFile(fileDir)
+			case "enter":
+				pathToOpen := m.currentDir + m.files[m.cursor]
+				v, err := IsDirectory(pathToOpen)
+				if err != nil {
+					fmt.Printf("%v", err)
+				}
+				if v {
+					reloadDir(m, pathToOpen+"/")
+				} else {
+					// idk what to do for enter
+				}
+
+			case "esc":
+				d := goUp(m.currentDir)
+				reloadDir(m, d)
 			}
+		} else {
 
-		case "down", "j":
-			if m.cursor < len(m.files)-1 {
-				m.cursor++
+			switch msg.String() {
+			case "left", "h":
+				if m.confirmCursor != 0 {
+					m.confirmCursor = 0
+				}
+			case "right", "l":
+				if m.confirmCursor != 1 {
+					m.confirmCursor = 1
+				}
+			case "enter":
+				if m.confirmCursor == 0 {
+					deleteFile(m.currentDir + m.files[m.cursor])
+
+				}
+				m.viewState = Default
+				m.View()
 			}
-
-		case "o", " ":
-			fileDir := m.currentDir + m.files[m.cursor]
-			openFile(fileDir)
-		case "enter":
-			pathToOpen := m.currentDir + m.files[m.cursor]
-			v, err := IsDirectory(pathToOpen)
-			if err != nil {
-				fmt.Printf("%v", err)
-			}
-			if v {
-				m.cursor = 0
-				m.currentDir = pathToOpen + "/"
-				m.files = loadFiles(m.currentDir)
-
-			} else {
-				// idk what to do for enter
-			}
-
-		case "esc":
-			d := goUp(m.currentDir)
-			fmt.Print(d)
-			m.currentDir = d
-			m.files = loadFiles(d)
-			m.View()
 		}
-
 	}
-
 	return m, nil
 }
-func (m model) View() string {
+func defaultView(m model) string {
 	s := "Select File: \n\n"
 	s += m.currentDir + "\n"
 
@@ -142,6 +176,29 @@ func (m model) View() string {
 	}
 	s += "\nPress q to quit.\nPress o to open selected file\n"
 	return s
+}
+func confirmDeleteView(m model) string {
+	s := "Are you really sure you want to delete"
+	s += "\n" + m.currentDir + m.files[m.cursor] + "\n"
+	if m.confirmCursor == 0 {
+		s += ">"
+	}
+	s += "YES"
+	s += "		"
+	if m.confirmCursor == 1 {
+		s += ">"
+	}
+	s += "NO"
+	return s
+}
+func (m model) View() string {
+	switch m.viewState {
+	case Default:
+		return defaultView(m)
+	case ConfirmDelete:
+		return confirmDeleteView(m)
+	}
+	return "Loading..."
 }
 func main() {
 	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
